a3f946f6deb7675c76fb503ee6d743ccd0624fd4
saya <geraldiakmal612@gmail.com>
2024-10-09 09:00:45 +0700

    a

---
+++ SiakadWhile10.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+
+public class SiakadWhile10 {
+
+
+    public static void main(String[] args) {
+
+        Scanner masuk = new Scanner(System.in);
+        int jmlhmhsiswa,n=0;
+        double nilai;
+        System.out.print("masukan jjumlah mahasiswa : ");
+        jmlhmhsiswa = masuk.nextInt();
+
+
+        while (n < jmlhmhsiswa){
+            System.out.print("Masukan nilai mahasiswa ke-"+(n+1)+" :");
+            nilai = masuk.nextDouble();
+            if (nilai>=0 && nilai<=100){
+                if (nilai>80 && nilai<=100){
+                    System.out.println("Nilai Mahasiswa ke-"+(n+1)+" adalah A");
+                }
+                else if (nilai>80 && nilai<=100){
+                    System.out.println("Nilai Mahasiswa ke-"+(n+1)+" adalah B+");
+                }
+                else if (nilai>73 && nilai<=80){
+                    System.out.println("Nilai Mahasiswa ke-"+(n+1)+" adalah B");
+                }
+                else if (nilai>65 && nilai<=73){
+                    System.out.println("Nilai Mahasiswa ke-"+(n+1)+" adalah C+");
+                }
+                else if (nilai>60 && nilai<=65){
+                    System.out.println("Nilai Mahasiswa ke-"+(n+1)+" adalah C");
+                }
+                else if (nilai>50 && nilai<=60){
+                    System.out.println("Nilai Mahasiswa ke-"+(n+1)+" adalah D");
+                }
+                else if (nilai>39 && nilai<=50){
+                    System.out.println("Nilai Mahasiswa ke-"+(n+1)+" adalah E");
+                }
+            }
+            else{
+                System.out.println("Nilai tidak valid.Masukan lagi nilai yang valid!");
+            }
+            n++;
+
+        }
+        masuk.close();
+        
+    }
+}
