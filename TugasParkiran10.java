3c3c2bd383f2bada175463dcda2b10f66e12bc98
saya <geraldiakmal612@gmail.com>
2024-10-11 22:15:33 +0700

    kursi

--- TugasParkiran10.java
+++ TugasParkiran10.java
@@ -1,46 +1,41 @@
 import java.util.Scanner;
 public class TugasParkiran10 {
     public static void main(String[] args) {
-    
-        int pilihanuser,durasiparkir;
-        double hargamobil= 3000,hargamotor = 2000,total=0;
-        do{
-            Scanner mlebu = new Scanner(System.in);
-            System.out.println("Masukan jenis kendaraan");
-            System.out.println("1.Mobil");
-            System.out.println("2.Motor");
-            System.out.println("3.Keluar");
-            System.out.print("Masukan pilihan : ");
-            pilihanuser = mlebu.nextInt();
-            if (pilihanuser== 1 || pilihanuser==2 || pilihanuser==0){
-                if (pilihanuser == 1){
-                    System.out.print("masukan durasi parkir anda : ");
-                    durasiparkir = mlebu.nextInt();
-                    if (durasiparkir>5){
-                        total += 12500;
+        int jenis,durasi = 0,total = 0;
+        Scanner mlebu = new Scanner(System.in);
+        do {
+            
+
+            System.out.println("1.Mobil ");
+            System.out.println("2.Motor ");
+            System.out.println("0.Metu");
+            System.out.print("Masukan jenis kendaraan : ");
+            jenis = mlebu.nextInt();
+            System.out.print("Masukan Durasi Parkir : ");
+            durasi = mlebu.nextInt();
+            
+            if (jenis == 1 || jenis == 2){
+                if(durasi > 5 ){
+                    total += 12500;
+                }
+                else{
+                    if (jenis == 1){
+                        total += durasi*3000;
+                    }
+                    else if(jenis == 2){
+                        total += durasi*2000; 
                     }
                     else{
-                        if (pilihanuser == 1){
-                            if(total == 0 ){
-                                total += hargamobil;
-                            }
-                            else{
-                            total += durasiparkir*3000;
-                            }
-                        }
-                        else if(pilihanuser == 2){
-                            if (total == 0){
-                                total += hargamotor;
-                            }
-                            total += durasiparkir*2000;
-                        }
+    
                     }
                 }
-            } 
-            else{
-                System.out.println("yang bener aja");
             }
-        }while(true);
+        }while(jenis != 0);
+        System.out.println("Total biaya parkir anda adalah Rp"+total);
+        mlebu.close();
+
+        
 
     }
 }
+    
